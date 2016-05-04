e9ded76d2c6b585e51e5b0c833f4c8225fe0dcd5
gtarnowski <g.tarnowski@proinserv.pl>
2016-05-04 22:57:27 +0200

    fs

---
+++ js/carpenter.js
@@ -0,0 +1,37 @@
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
+
+$('.showGallery').click(function(){
+   $('.full-gallery').toggle('show') 
+});
+
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
+
+$(function(){
+    $('.big-box').on('click',function(){
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        $('#imagemodal').modal('show');   
+    })
+})
